b5e3426b7e000ee1e8d796e8355d36c4f06acd2c
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-10-08 01:54:51 -0300

    Update compat.c: formatting

--- source/compat.c
+++ source/compat.c
@@ -49,11 +49,9 @@
 
 void sr_TraceLog(const char *szFile, const char *szTraceMsg, ...)
 {
-  if (szTraceMsg)
-  {
+  if (szTraceMsg) {
     FILE *hFile = hb_fopen(szFile ? szFile : "trace.log", "a");
-    if (hFile)
-    {
+    if (hFile) {
       va_list ap;
       va_start(ap, szTraceMsg);
       vfprintf(hFile, szTraceMsg, ap);
