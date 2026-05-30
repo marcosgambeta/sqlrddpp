c829631fdfdebfd057cee0b2e80752cb65064e01
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2026-05-30 19:16:13 -0300

    Update source/mariadb/sr_mariadb_bind.c: fix SR_MARIADBFINISH function

--- source/mariadb/sr_mariadb_bind.c
+++ source/mariadb/sr_mariadb_bind.c
@@ -140,20 +140,24 @@ HB_FUNC_STATIC(SR_MARIADBFINISH)
 {
   GET_MARIADB_SESSION(session, 1);
 
-  if (session == SR_NULLPTR || session->dbh == SR_NULLPTR) {
+  if (session == SR_NULLPTR) {
     hb_ret();
     return;
   }
 
-  mysql_close(session->dbh);
-
-  hb_xfree(session);
-  if (s_iConnectionCount > 0) {
-    s_iConnectionCount--;
+  if (session->dbh != SR_NULLPTR) {
+    mysql_close(session->dbh);
+    if (s_iConnectionCount > 0) {
+      s_iConnectionCount--;
+    }
   }
+
   if (s_iConnectionCount == 0) {
     mysql_library_end();
   }
+
+  hb_xfree(session);
+
   hb_ret();
 }
 
